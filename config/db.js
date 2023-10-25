af1f1419cd58f784d204ebe8bbacf99053bf7a68
Paschal A. Ogba <paxbond01@gmail.com>
2023-10-26 00:01:49 +0100

    create new user and save in database

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // connect to mongodb atlas
 const dbUrl = process.env.MONGODB_URI;
-mongoose.connect(dbUrl, { useNewUrlParser: true });
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 let connection = mongoose.connection;
 connection.on('connected', () => console.log(`Mongoose connected App to ${dbUrl}`));
 connection.once('error', (err) => { throw err });

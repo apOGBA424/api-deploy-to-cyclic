7a1ca2a6f5376e0fae75421ef6f645ee7014bc4a
Paschal A. Ogba <paxbond01@gmail.com>
2023-10-25 05:45:56 +0100

    app connected to MongoDB Atlas server

---
+++ config/db.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+
+// connect to mongodb atlas
+const dbUrl = process.env.MONGODB_URI;
+mongoose.connect(dbUrl, { useNewUrlParser: true });
+let connection = mongoose.connection;
+connection.on('connected', () => console.log(`Mongoose connected App to ${dbUrl}`));
+connection.once('error', (err) => { throw err });
+
+
+module.exports = connection;
